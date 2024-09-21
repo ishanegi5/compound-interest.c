9b6ee6e89cfe563f2b6aac68cf7a06c464ed04a2
Isha Negi <itsishanegi@gmail.com>
2024-09-22 02:58:18 +0530

    Add files via upload

---
+++ compound-interest.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+#include<math.h>
+
+
+void main()
+
+{
+    int a,b,p,r,t,n,x;
+    
+printf("enter p,r,t,n: ");
+    
+scanf("%d %d %d %d",&p,&r,&t,&n);
+    
+b=(1+(r/t));
+    
+x=pow(b,(n*t));
+    
+a=p*x;
+    
+    
+printf("compound interest: %d",a);
+    
+    
+
+}
